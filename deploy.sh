1d1b44bc2fa98e14e38b8c98cc75f7c2f34ddcdc
root <root@localhost.localdomain>
2018-07-11 01:34:17 -0400

    added clean up to deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,8 @@ if [ -e artifacts/index.zip ]; then
   echo "ok"
   aws lambda update-function-code --function-name student-response-check --zip-file fileb://artifacts/index.zip
 else
-  echo "nok"
+  echo "Please run the build script first to create the artifact(s)."
 fi
 
+#clean up
+rm -rf ./artifacts
